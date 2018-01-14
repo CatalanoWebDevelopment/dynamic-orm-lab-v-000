c5a6b178244f813a6c2915668ff111014afd377a
Parker Catalano <catalano.work@gmail.com>
2018-01-14 21:40:25 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -55,7 +55,7 @@ class InteractiveRecord
     DB[:conn].execute(sql)
   end
 
-  def self.find_by(options = {})
+  def self.find_by()
     sql = "SELECT * FROM #{self.table_name} WHERE "
 
     DB[:conn].execute(sql)
